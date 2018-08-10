830eb15a8ab1dad706d8c24ef3b65dd77a5e79db
Richard <richard.dimalanta@gmail.com>
2018-08-10 21:51:23 +1000

    delete the link icon.

--- src/components/MediaCard/index.js
+++ src/components/MediaCard/index.js
@@ -5,6 +5,8 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 
 const styles = theme => ({
   card: {
@@ -51,6 +53,9 @@ function MediaCard(props) {
             </Typography>
           </CardContent>
         </div>
+        <IconButton>
+          <CloseIcon />
+        </IconButton>
       </Card>
     </div>
   );
