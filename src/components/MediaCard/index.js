6dd601ec8ff4ec7286526551786ad368ad3852b8
Richard <richard.dimalanta@gmail.com>
2018-08-11 21:01:05 +1000

    fixed clicking on link cards.

--- src/components/MediaCard/index.js
+++ src/components/MediaCard/index.js
@@ -33,30 +33,36 @@ function MediaCard(props) {
   const { classes, title, description, url, author, publisher, id } = props;
   return (
     <div className="MediaCard">
-      <Card
-        className="card"
-        onClick={() => {
-          openInNewTab(url);
-        }}
-      >
-        <CardMedia className={classes.cover} image={props.image} />
-        <div className={classes.details}>
-          <CardContent className={classes.content}>
-            <Typography variant="title">
-              {title.length > 25 ? title.substring(0, 25) + "..." : title}
-            </Typography>
-            <Typography variant="subheading" color="textSecondary" gutterBottom>
-              By {author === null ? publisher : author}
-            </Typography>
-            <Typography component="p" gutterBottom>
-              {description.length > 60
-                ? description.substring(0, 60) + "..."
-                : description}
-            </Typography>
-            <Typography component="p" variant="body2">
-              {url}
-            </Typography>
-          </CardContent>
+      <Card className="card">
+        <div
+          className="content"
+          onClick={() => {
+            openInNewTab(url);
+          }}
+        >
+          <CardMedia className={classes.cover} image={props.image} />
+          <div className={classes.details}>
+            <CardContent className={classes.content}>
+              <Typography variant="title">
+                {title.length > 25 ? title.substring(0, 25) + "..." : title}
+              </Typography>
+              <Typography
+                variant="subheading"
+                color="textSecondary"
+                gutterBottom
+              >
+                By {author === null ? publisher : author}
+              </Typography>
+              <Typography component="p" gutterBottom>
+                {description.length > 60
+                  ? description.substring(0, 60) + "..."
+                  : description}
+              </Typography>
+              <Typography component="p" variant="body2">
+                {url}
+              </Typography>
+            </CardContent>
+          </div>
         </div>
         <IconButton>
           <CloseIcon
