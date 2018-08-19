3d8130fbb3496b54ad66f9e61f4cbd00c1480eb5
Richard <richard.dimalanta@gmail.com>
2018-08-19 13:00:51 +1000

    remove unused styles.

--- src/components/SimpleDrawer/index.js
+++ src/components/SimpleDrawer/index.js
@@ -13,25 +13,8 @@ import CategoryList from "../CategoryList";
 import "./SimpleDrawer.css";
 
 const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    height: 430,
-    zIndex: 1,
-    overflow: "hidden",
-    position: "relative",
-    display: "flex",
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-  },
   drawerPaper: {
     position: "relative",
-  },
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing.unit * 3,
-    minWidth: 0, // So the Typography noWrap works
   },
   toolbar: theme.mixins.toolbar,
 });
