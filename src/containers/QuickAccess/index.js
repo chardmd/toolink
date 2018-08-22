f94528bb6ff4f1bdd43f1913de1a6d40fa00f276
Richard <richard.dimalanta@gmail.com>
2018-08-22 22:18:56 +1000

    added onboarding image in quick access

--- src/containers/QuickAccess/index.js
+++ src/containers/QuickAccess/index.js
@@ -6,23 +6,73 @@
 
 import React from "react";
 import { connect } from "react-redux";
+import Button from "@material-ui/core/Button";
+import Icon from "@material-ui/core/Icon";
 
-import { defaultAction } from "./actions";
-
+import tourImage from "../../assets/tour.svg";
 import "./QuickAccess.css";
 
+//components
+import FormDialog from "../../components/FormDialog";
+
+//actions
+import { getCategories } from "../Categories/actions";
+import { saveLink } from "../Links/actions";
+
 export class QuickAccess extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isActive: false,
+    };
+
+    this.onToggleStatus = this.onToggleStatus.bind(this);
+  }
+
+  onToggleStatus(status) {
+    this.setState({
+      isActive: status,
+    });
+  }
+
   render() {
-    return <div />;
+    return (
+      <div className="QuickAccess">
+        <div className="onboarding">
+          <img src={tourImage} alt="tour" className="noFavImage" />
+          <h1>Get started with adding new links</h1>
+          <Button
+            variant="extendedFab"
+            color="secondary"
+            size="large"
+            onClick={() => {
+              this.onToggleStatus(true);
+            }}
+          >
+            <Icon>link</Icon>
+            &nbsp;
+            <span>Add Link</span>
+          </Button>
+          <FormDialog
+            isActive={this.state.isActive}
+            toggleStatus={this.onToggleStatus}
+            categories={this.props.categories}
+            saveLink={this.props.saveLink}
+          />
+        </div>
+      </div>
+    );
   }
 }
 
 const mapStateToProps = state => ({
-  myProperty: state.app.myProperty,
+  categories: state.categories,
 });
 
 const mapDispatchToProps = dispatch => ({
-  defaultAction: () => dispatch(defaultAction()),
+  saveLink: link => dispatch(saveLink(link)),
+  getCategories: () => dispatch(getCategories()),
 });
 
 export default connect(
